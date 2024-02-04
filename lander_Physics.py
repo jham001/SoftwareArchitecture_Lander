c1b5fa138ceac4eb45aeab9ec06b1a400334f292
Joshuaham123 <ham4josh@proton.me>
2024-02-04 17:33:54 -0500

    Added sample method and equation

--- lander_Physics.py
+++ lander_Physics.py
@@ -3,7 +3,22 @@
 
 # Sophie was here
 
+h = float(10000)
+hInit = float(10000)
+vInit = float(-1000)
+vInit = 0
+a = float(0.1)
+t = 0
+
+def updatePosition(h, hInit, vInit, t, a):
+    h = hInit + vInit*t + 0.5*a*(t)**2
+    return h
+
+def main(h, hInit, vInit, t, a):
+    while True:
+        t += 1
+        h = updatePosition(h, hInit, vInit, t, a)
+        print(h)
 
 
-def updatePosition():
-    pass
+main(h, hInit, vInit, t, a)
