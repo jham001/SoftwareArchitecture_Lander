b22c7ea21990616889eee468bd3b6c71533f664c
Joshuaham123 <ham4josh@proton.me>
2024-02-04 17:40:05 -0500

    Added acceleration

--- lander_Physics.py
+++ lander_Physics.py
@@ -3,22 +3,30 @@
 
 # Sophie was here
 
-h = float(10000)
-hInit = float(10000)
-vInit = float(-1000)
-vInit = 0
-a = float(0.1)
+h = 100000
+hInit = 100000
+vInit = -100
+a = 0
 t = 0
+g = 3.72076 # m/s^2 Mars
+A = 0
 
 def updatePosition(h, hInit, vInit, t, a):
     h = hInit + vInit*t + 0.5*a*(t)**2
     return h
 
-def main(h, hInit, vInit, t, a):
+def updateAcceleration(A, g):
+    a = A-g
+    return a
+
+
+
+def main(h, hInit, vInit, t, a, g, A):
     while True:
         t += 1
+        a = updateAcceleration(A, g)
         h = updatePosition(h, hInit, vInit, t, a)
         print(h)
 
 
-main(h, hInit, vInit, t, a)
+main(h, hInit, vInit, t, a, g, A)
