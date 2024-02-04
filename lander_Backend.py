0f1de3fcae8b5942303a26357b744fd4db78bf3d
Joshuaham123 <ham4josh@proton.me>
2024-02-04 17:07:55 -0500

    Added Physics script

--- lander_Backend.py
+++ lander_Backend.py
@@ -1,3 +1,19 @@
 # libraries
 import sqlite3
-conn = sqlite3.connect('blackboard.db')
+
+# Connect to blackboard
+DATA = sqlite3.connect('blackboard.db')
+print("Connection established ..........")
+
+
+DATA.execute ('''CREATE TABLE CRICKETERS (
+   First_Name VARCHAR(255),
+   Last_Name VARCHAR(255),
+   Age int,
+   Place_Of_Birth VARCHAR(255),
+   Country VARCHAR(255)
+);''')
+
+# Close blackboard
+DATA.close()
+
