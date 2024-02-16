5a9053d3c954a118143783e66f2ef1056b4d0960
dnalywajko <dnalywajko001@student.franciscan.edu>
2024-02-15 22:13:24 -0500

    changed circle to rocket

--- lander_GUI.py
+++ lander_GUI.py
@@ -7,11 +7,11 @@ from random import randrange
 
 # variables
 startingHeight = 100000
-altitude = startingHeight
-fuel = 4000
-weight = 5000
-velocity = 200
-impactTime = 100
+Ialtitude = startingHeight
+Ifuel = 4000
+Iweight = 5000
+Ivelocity = 200
+IimpactTime = 100
 thrusterToggle = False
 parachuteReleased = False
 
@@ -20,15 +20,15 @@ psg.theme('DarkBlue13')
 
 # graph setup
 graph=psg.Graph(canvas_size=(300,900), graph_bottom_left=(0, -1000),
-    graph_top_right=((startingHeight/3), (startingHeight+startingHeight/100)),
+    graph_top_right=((startingHeight/3), (startingHeight+startingHeight/80)),
     background_color='black', enable_events=True, drag_submits=True, key='graph')
 # column1 setup with text and buttons
 column1 = [
-    [psg.Text("Altitude:"), psg.Text(str(altitude) + " m", key="altitudetxt")],
-    [psg.Text("Fuel Remaining:"), psg.Text(str(fuel) + " kg", key="fueltxt")],
-    [psg.Text("Total Weight:"), psg.Text(str(weight) + " kg", key="weighttxt")],
-    [psg.Text("Velocity:"), psg.Text(str(velocity) + " m/s", key="velocitytxt")],
-    [psg.Text("Time until Impact:"), psg.Text(str(impactTime) + " s", key="impacttimetxt")],
+    [psg.Text("Altitude:"), psg.Text(str(Ialtitude) + " m", key="altitudetxt")],
+    [psg.Text("Fuel Remaining:"), psg.Text(str(Ifuel) + " kg", key="fueltxt")],
+    [psg.Text("Total Weight:"), psg.Text(str(Iweight) + " kg", key="weighttxt")],
+    [psg.Text("Velocity:"), psg.Text(str(Ivelocity) + " m/s", key="velocitytxt")],
+    [psg.Text("Time until Impact:"), psg.Text(str(IimpactTime) + " s", key="impacttimetxt")],
     [psg.Button("Thrusters")],
     [psg.Button("Parachute")],
 ]
@@ -36,14 +36,19 @@ column1 = [
 column2 = [
     [graph]
 ]
-# layout puts the window layout together an window instantiates the window
+# layout puts the window layout together and window instantiates the window
 layout = [[psg.Column(column1), psg.VSeparator(), psg.Column(column2)]]
 window = psg.Window('Penguin Lander', layout, finalize=True)
 
 # objects on the graph
-rocket = graph.draw_circle(((startingHeight/6),startingHeight), 1000,
+rocketTop = graph.DrawPolygon(((startingHeight/6-500, startingHeight + 1000),
+    (startingHeight/6+500, startingHeight + 1000), (startingHeight/6, startingHeight + 1800)),
     fill_color='red', line_color='white')
-graph.bring_figure_to_front(rocket)
+rocketMiddle = graph.DrawRectangle((startingHeight/6-500, startingHeight + 1000),
+    (startingHeight/6+500, startingHeight-1000), fill_color='red', line_color='white')
+rocketBottom = graph.DrawPolygon(((startingHeight/6-900, startingHeight - 1600),
+    (startingHeight/6, startingHeight + 1900), (startingHeight/6+950, startingHeight - 1600),
+    (startingHeight/6, startingHeight-1100)), fill_color='red', line_color='white')
 moon = graph.draw_rectangle(((startingHeight/3),(startingHeight/30)),
     (0,(-startingHeight/30)), line_color='grey', fill_color='grey')
 for stars in range(300):
@@ -53,38 +58,44 @@ for stars in range(300):
 for craters in range(35):
     x,y = randrange(int(startingHeight/3)), startingHeight/40-randrange(int(startingHeight/30))
     crater = graph.draw_circle((x, y), (randrange(int(6))+3)*100, line_color='#333333',  fill_color='#767676')
-
+graph.bring_figure_to_front(rocketBottom)
+graph.bring_figure_to_front(rocketTop)
+graph.bring_figure_to_front(rocketMiddle)
 # functions we call every 1 second
 def updateAltitude():
     # get new altitude
-    
+    altitude = 42
     # update altitude
     window['altitudetxt'].update(str(altitude) + " m")
 def updateFuel():
     # get new fuel
-    
+    fuel = 69
     # update fuel
     window['fueltxt'].update(str(fuel) + " kg")
 def updateWeight():
     # get new weight
-    
+    weight = 96
     # update weight
     window['weighttxt'].update(str(weight) + " kg")
 def updateVelocity():
     # get new velocity
-    
+    velocity = 456
     # update velocity
     window['velocitytxt'].update(str(velocity) + " m/s")
 def updateImpactTime():
     # get new impact time
-    
+    impactTime = 789
     # update impact time
     window['impacttimetxt'].update(str(impactTime) + " s")
 def moveRocket():
     if thrusterToggle == True:
-        graph.MoveFigure(rocket, 0,-100)
+        graph.MoveFigure(rocketTop, 0, -100)
+        graph.MoveFigure(rocketMiddle, 0, -100)
+        graph.MoveFigure(rocketBottom, 0, -100)
     else:
-        graph.MoveFigure(rocket, 0,-200)
+        graph.MoveFigure(rocketTop, 0, -200)
+        graph.MoveFigure(rocketMiddle, 0, -200)
+        graph.MoveFigure(rocketBottom, 0, -200)
 
 # infinite loop
 while True:
