0ce1dde627f174e22ae63a99e5020543b7b6278d
Joshuaham123 <ham4josh@proton.me>
2024-03-07 20:13:14 -0500

    Fixed acceleration and thrust equations

--- lander_Physics.py
+++ lander_Physics.py
@@ -1,17 +1,13 @@
 import lander_Backend
 
 
-t = 0
-
-dt = 1 # (sec)
-
 # Gravitational Acceleration
 def getGravitationalAcceleration():
     G_moon = 6.67430*10**(-11) # Moon (m^3/(kg*s^2))
     m_moon = 7.34767309*10**22 # Moon kg
     r_moon = 1740000.0 # Moon (m)
     h = lander_Backend.getAltitude() # (m)
-    g_moon = G_moon * m_moon / ((r_moon + h)**2)
+    g_moon = G_moon * m_moon / ((r_moon + h)**2) # (m/s^2)
     
     return g_moon
 
@@ -20,21 +16,14 @@ def getGravitationalAcceleration():
 def getCurrentAcceleration(thrusterToggle):
     # Net Force= Force of thrust - Force of gravity
     F_Thrust = 16000 * thrusterToggle # (N) 
-    g_moon = getGravitationalAcceleration() # (t)
+    g_moon = getGravitationalAcceleration() # (m/s^2)
     m_lander = lander_Backend.getLanderMass() # (kg)
     m_fuel = lander_Backend.getFuelMass() # (kg)
 
-    total_mass = m_lander + m_fuel
-    specificImpulse = 311
-    consumption = F_Thrust*dt/specificImpulse
-    consumption = 10
+    total_mass = m_lander + m_fuel # (kg)
+    consumption = 10 # (kg/s)
 
-    F_g = g_moon * total_mass
-    Force_Net = F_Thrust - F_g
-
-    if (m_lander - 0.5 * m_fuel) != 0:
-        a_t = Force_Net / (m_lander - 0.5 * consumption) 
-        return a_t
+    return (F_Thrust/(m_lander - 0.5 * consumption)) - g_moon # (m/s^2)
 
 
 # Current Lander Velocity
@@ -64,13 +53,9 @@ def getCurrentAltitude(thrusterToggle):
     return h_t_after_thrust
 
 
-
-# Altitude, Fuel, Weight, Velocity, Impact till Time, Displacement
-
 def getFuel():
-    F_Thrust = 16000
-    specificImpulse = 311
-    consumption = F_Thrust*dt/specificImpulse
+    F_Thrust = 1000 # (N)
+    consumption = 10 # (kg)
     fuel = lander_Backend.getFuelMass() - consumption
     return fuel
 
