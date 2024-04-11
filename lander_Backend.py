3f3adda6c1f7102f9a65d68d35121d9f1537ab49
dnalywajko <dnalywajko001@student.franciscan.edu>
2024-04-11 16:59:45 -0400

    minor comment refactor

--- lander_Backend.py
+++ lander_Backend.py
@@ -1,5 +1,4 @@
-# libraries
-import sqlite3
+import sqlite3 # database library
 
 table_name = f""  # Declare table_name as a global variable
 dbConnection = sqlite3.connect('blackboard.db') # Connect to blackboard as a global variable
@@ -164,25 +163,3 @@ def close():
     global cursor
     cursor.close()
     dbConnection.close()
-
-# DB
-    # Table Trial #1
-    # Table Trial #2
-
-# Make DB if it does not exist
-# Get number of latest trial
-# Make a new Table with new trial number
-    # This is the Table we will use
-
-# Table stores Altitude, Fuel, Weight, Velocity, Time till Impact, Displacement as columns
-# Each row is a new timestamp
-#   ex. t=0, t=1, t=2
-
-    # New Table will hold initial values, Dont worry about them too much for now, can use placeholders.
-    # Altitude = 100000, Fuel = placeholder, Weight = placeholder, Velocity = placeholder, Time till Impact = 1, Displacement = 0
-
-
-
-# There should be a method to set them all
-# There should be methods to get each individualy (Latest value for now)
-  
