708c5b082014e36f5a231f8c610b6607fdf2cc5f
ngbalarezo <143428049+ngbalarezo@users.noreply.github.com>
2024-04-29 16:26:42 -0400

    Added a few more comments.

--- lander_Backend.py
+++ lander_Backend.py
@@ -1,27 +1,31 @@
 import sqlite3 # database library
 
-table_name = f""  # Declare table_name as a global variable
-dbConnection = sqlite3.connect('blackboard.db') # Connect to blackboard as a global variable
+#declare table name as a global variable
+table_name = f""
+#connect to blackboard using a global variable
+dbConnection = sqlite3.connect('blackboard.db')
+#connect cursor as a global variable
 cursor = dbConnection.cursor() # Connect cursor as a global variable
 
+#function to create new table in the database
 def newTable(time_elapsed, h, v, m_fuel, m_lander, displacement, acceleration, impactTime):
-    # Declare that you are modifying the global variables within this function
+    #declare that you are modifying the global variables within this function
     global table_name
     global dbConnection
     global cursor
 
-    # Execute a query to get the number of tables
+    #execute a query to get the number of tables
     cursor.execute("SELECT COUNT(*) FROM sqlite_master WHERE type='table';")
 
-    # Fetch the result
+    #fetch the result
     num_tables = cursor.fetchone()[0]
 
-    # Print the number of tables
+    #print the number of tables
     print("Number of tables in the database:", num_tables)
     table_name = f"blackboard_display_data{num_tables}"
     print(table_name)
 
-    # create a new table
+    #create a new table
     cursor.execute(f'''CREATE TABLE IF NOT EXISTS {table_name}
     (
     time_elapsed_sec float PRIMARY KEY,
@@ -34,7 +38,7 @@ def newTable(time_elapsed, h, v, m_fuel, m_lander, displacement, acceleration, i
     time_till_impact float
     );''')
 
-    # Add Initial values
+    #add Initial values
     Time = time_elapsed
     Altitude = h
     Velocity = v
@@ -44,48 +48,51 @@ def newTable(time_elapsed, h, v, m_fuel, m_lander, displacement, acceleration, i
     Acceleration = acceleration
     TTI = impactTime
 
-    # Construct the SQL command for insertion
+    #construct the SQL command for insertion
     sqlCommand = f"INSERT INTO {table_name} (time_elapsed_sec, altitude, velocity, fuel_mass, lander_mass, displacement, acceleration, time_till_impact) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
 
-    # Execute the insertion with parameterized values
+    #execute the insertion with parameterized values
     cursor.execute(sqlCommand, (Time, Altitude, Velocity, Fuel, LanderMass, Displacement, Acceleration, TTI))
 
-    # Commit the changes
+    #commit the changes
     dbConnection.commit()    
 
+#getter function that returns altitude from database
 def getAltitude():
-    # Declare that you are modifying the global variables within this function
+    #declare that you are modifying the global variables within this function
     global table_name
     global dbConnection
     global cursor
-
-    # Value from last row, altitude column
+    #value from last row, altitude column
     sqlCommand = f"SELECT altitude FROM {table_name} ORDER BY time_elapsed_sec DESC LIMIT 1"
     cursor = dbConnection.execute(sqlCommand)
-    
+    #initialize a result variable
     result = 0
+    #store latest row in query result 
     row = cursor.fetchone()
+    #if the row exists then store value of row 0 in the result variable 
     if row:
         result = row[0]
-
+    #return result
     return result  
 
+#getter function that returns velocity from database
 def getVelocity():
-    # Declare that you are modifying the global variables within this function
+    #declare that you are modifying the global variables within this function
     global table_name
     global dbConnection
     global cursor
-
-    # Value from last row, velocity column
+    #value from last row, velocity column
     sqlCommand = f"select velocity from {table_name}"
     cursor = dbConnection.execute(sqlCommand)
-    
+    #initialize a result variable
     result = 0
+    #store row 0 result in the result variable
     for row in cursor:
-        result =  row[0]
-
+        result = row[0]
+    #return row
     return result
-
+#!FIXME: LEFT OFF HERE!!!!!!!!!!!!
 def getFuelMass():
     # Declare that you are modifying the global variables within this function
     global table_name
